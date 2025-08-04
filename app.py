93ee56eab3f9ed42b55c556bbacd7bf183f3ba4b
sreelekshmir7 <sreelekshmirmohan@gmail.com>
2025-08-04 10:24:11 +0530

    added docker file and dockerignore file

--- app.py
+++ app.py
@@ -71,4 +71,4 @@ def predict():
         return render_template('input.html', user_info=user_info, error_message=error_message)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
